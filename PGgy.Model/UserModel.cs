ab682829c86c8701d72e75033d41a8458ab69d24
rushin-gh <rushin.mailme@gmail.com>
2025-04-17 01:11:17 +0530

    WIP Login Page

---
+++ PGgy.Model/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using PGgy.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGgy.Model
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+
+        public UserTypeEnum UserType { get; set; }
+    }
+}
